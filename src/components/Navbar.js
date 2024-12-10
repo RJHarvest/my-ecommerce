9ad7d7bf5967d221f58d71ddb8e3cfa22d5bb905
Russell Gapasin <rjharvest@hotmail.com>
2024-12-11 01:57:11 +0800

    feat: Change the logo color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <Link to={`/${BASE_ROUTE}`} className="text-xl font-bold text-primary">
+            <Link to={`/${BASE_ROUTE}`} className="text-xl font-bold text-secondary">
               Your Logo
             </Link>
           </div>
